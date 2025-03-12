f10c677c44bdb4d858b7dd1339266070fa2e60d9
cheikh ibrahima dieng <barhamadieng66@gmail.com>
2025-03-12 14:54:39 +0000

    first commit

---
+++ src/main/java/com/codev13/electrosign13back/Electrosign13BackApplication.java
@@ -0,0 +1,13 @@
+package com.codev13.electrosign13back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Electrosign13BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Electrosign13BackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codev13/electrosign13back/Electrosign13BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codev13.electrosign13back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Electrosign13BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
